404c2794af584e72c1c8190475c57d48cfb6d05b
Edgars Jaunzems <oxohezeci622@gmail.com>
2026-04-15 17:54:06 +0200

    Initial Commit

---
+++ JB Pato Atlas/JB_Pato_AtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  JB_Pato_AtlasApp.swift
+//  JB Pato Atlas
+//
+//  Created by Денис on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct JB_Pato_AtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
